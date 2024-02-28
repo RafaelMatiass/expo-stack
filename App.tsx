50189f38f4655eecaed9da8c3a837c943eeb1b5c
Rafael Matias <rafaeldavi8086@gmail.comgit config --global user.email rafaeldavi8086@gmail.comexitgit config --lits>
2024-02-28 07:22:56 -0300

    Initial commit
    Generated by create-expo-stack 2.0.0.

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import './global.css';
+
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = {
+  container: 'flex flex-1 items-center justify-center bg-white',
+};
